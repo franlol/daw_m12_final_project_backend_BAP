cac3ba0bcd40d9f18d146c8f44c9375594e2ede5
franlol <fraaaaan@gmail.com>
2019-10-19 18:12:20 +0200

    :card_file_box: - Signup created. Some todo left

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(  {
       type: String,
       required: true
     },
-    cognoms: {
+    surname: {
       type: String,
       required: true
     },
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,54 @@ const express = require('express');
 const User = require('../database/models/User');
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  
+router.post('/signup', async (req, res, next) => {
+  // POST = body
+  // GET = query
+  // PARAMETRE = params
+  const { userName, name, surname, email, password, location } = req.body;
+
+  if (!userName || !name || !surname || !email || !password || !location) {
+    res.status(404); //TODO
+    res.json({
+      message: 'All fields are required'
+    });
+  }
+
+  // Todo check fields
+
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(404); //TODO
+      res.json({
+        message: 'Email already taken'
+      });
+    }
+    const newUser = {
+      userName,
+      name,
+      surname,
+      email,
+      password,
+      location
+    }
+    // TODO encrypt password
+
+    const userCreated = await User.create(newUser);
+    //TODO crear token
+    const token = 'asdasd';
+
+    res.status(200);
+    res.json({
+      message: 'User created',
+      token
+    });
+
+  } catch (err) {
+    next(err);
+  }
+
 });
 
 
