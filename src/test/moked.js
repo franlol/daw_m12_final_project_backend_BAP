9a45c5132531fa714c9caac79d08b59223b918ed
franlol <fraaaaan@gmail.com>
2019-11-01 17:06:59 +0100

    :building_construction: - Adding JEST (testing) architecture

---
+++ src/test/moked.js
@@ -0,0 +1,23 @@
+const User = require('../database/models/User');
+const bcrypt = require('bcrypt');
+
+const createUserTest = async () => {
+  const testUser = await User.findOne({ email: process.env.TEST_EMAIL })
+  if (!testUser) {
+    const satRounds = 10;
+    const hashedPassword = await bcrypt.hash(process.env.TEST_PASSWORD, satRounds);
+
+    const mokedUser = {
+      username: 'testuser',
+      name: 'User for Jest tests',
+      surname: 'test',
+      email: process.env.TEST_EMAIL,
+      password: hashedPassword,
+      cp: '00000',
+      location: [0, 0]
+    }
+    await User.create(mokedUser);
+  }
+}
+
+module.exports = createUserTest;
