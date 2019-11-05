b88bd9af0ba4d5d2e68685c779d29655b45e15cf
franlol <fraaaaan@gmail.com>
2019-11-05 18:35:04 +0100

    :zap: - Improving test descriptions

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -22,35 +22,49 @@ describe('BAP TESTS', () => {
     await db.close();
   });
 
-  // API testing
-  it('Should run the API and give a response.', apiTests.apiIsRunning);
+  describe('# API Tests', () => {
+    it('- Should run the API and give a response.', apiTests.apiIsRunning);
+  });
 
-  // Auth Testing
-  // Login
-  it('Should not login a unexistent username.', authTests.loginUnexistentUserame);
-  it('Should not login with invalid credentials.', authTests.loginWithIncorrectCredentials);
-  it('Should not login with empty fields.', authTests.loginWithEmptyFields);
-  it('Should login a moked user.', authTests.loginMokedUser);
-  // Signup
-  it('Should not signup a new user with invalid password.', authTests.signupWithInvalidPassword);
-  it('Should not sign up a existing user.', authTests.signupExistingUser);
-  it('Should not sign up with empty fields.', authTests.signupWithEmptyFields);
-  it('Should signup a new user', authTests.signUpNewUser);
-  // Logout
-  it('Should logout a user with valid token', authTests.logoutUserWithValidToken);
-  it('Should not logout a user with invalid token', authTests.logoutUserWithInvalidToken);
+  describe('# AUTH Tests', () => {
+    describe('- /Login', () => {
+      it('- Should not login a unexistent username.', authTests.loginUnexistentUserame);
+      it('- Should not login with invalid credentials.', authTests.loginWithIncorrectCredentials);
+      it('- Should not login with empty fields.', authTests.loginWithEmptyFields);
+      it('- Should login a moked user.', authTests.loginMokedUser);
+    });
+    
+    describe('- /Signup', () => {
+      it('- Should not signup a new user with invalid password.', authTests.signupWithInvalidPassword);
+      it('- Should not sign up a existing user.', authTests.signupExistingUser);
+      it('- Should not sign up with empty fields.', authTests.signupWithEmptyFields);
+      it('- Should signup a new user', authTests.signUpNewUser);
+    });
 
-  // Profile Testing
-  // Get /profile
-  it('Should return a full user object when access /profile@get with valid token.', profileTests.getOwnProfileWithValidToken);
-  it('Should not return a user data when access /profile@get without token.', profileTests.getOwnProfileWithoutToken);
-  it('Should not return a user data when access /profile@get with expired token.', profileTests.getOwnProfileWithExpiredToken);
-  it('Should not return a user data when access /profile@get with invalid token.', profileTests.getOwnProfileWithInvalidToken);
-  // Post /profile
-  it('Should update testing user.', profileTests.updateTestingUser);
-  it('Should not update testing user with invalid zipcode.', profileTests.updateTestingUserWithInvalidZipcode);
-  // Get /profile/:id
-  it('Should return user information when accessing /profile/:id with valid token.', profileTests.getProfileByIdWithValidToken);
-  it('Should not return user information when accessing /profile/:id with invalid token.', profileTests.getProfileByIdWithInvalidToken);
-  it('Should not return user information when accessing /profile/:id with invalid ID.', profileTests.getProfileWithInvalidId);
+    describe('- /Logout', () => {
+      it('- Should logout a user with valid token', authTests.logoutUserWithValidToken);
+      it('- Should not logout a user with invalid token', authTests.logoutUserWithInvalidToken);
+    });
+  });
+
+  describe('# PROFILE Testing', () => {
+    describe('- /Profile @ GET', () => {
+      it('- Should return a full user object when access /profile@get with valid token.', profileTests.getOwnProfileWithValidToken);
+      it('- Should not return a user data when access /profile@get without token.', profileTests.getOwnProfileWithoutToken);
+      it('- Should not return a user data when access /profile@get with expired token.', profileTests.getOwnProfileWithExpiredToken);
+      it('- Should not return a user data when access /profile@get with invalid token.', profileTests.getOwnProfileWithInvalidToken);
+    });
+
+    describe('- /Profile @ POST', () => {
+      it('- Should update testing user.', profileTests.updateTestingUser);
+      it('- Should not update testing user with invalid zipcode.', profileTests.updateTestingUserWithInvalidZipcode);
+    });
+
+    describe('- /Profile/:id @ GET', () => {
+      it('- Should return user information when accessing /profile/:id with valid token.', profileTests.getProfileByIdWithValidToken);
+      it('- Should not return user information when accessing /profile/:id with invalid token.', profileTests.getProfileByIdWithInvalidToken);
+      it('- Should not return user information when accessing /profile/:id with invalid ID.', profileTests.getProfileWithInvalidId);
+    });
+  });
+
 });
