315f7add7a3de6c94e8459a377f1b950982bc9be
franlol <fraaaaan@gmail.com>
2019-11-06 18:40:28 +0100

    :ambulance: - Updating deprecated session options

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(session({ secret: 's3cr37' })); // TODO generate new key, and put in envs
+app.use(session({ secret: 's3cr37', saveUninitialized: true, resave: true })); // TODO generate new key, and put in envs
 // app.use(morgan('dev'));
 dotenv.config({ path: __dirname + '/.env' });
 app.use(routes);
