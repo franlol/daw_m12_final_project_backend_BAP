ea9a915d2b75bdbbdc282fa10c579b242d4524ec
xavixanxe <xavixanxe@gmail.com>
2019-12-23 10:15:40 +0100

    :card_file_box: - Adding Message and Availability models

---
+++ src/database/models/Message.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const messageSchema = new Schema(
+    {
+        postId: {
+            type: ObjectId,
+            ref: 'Post',
+            required: true
+        },
+        userId: {
+            type: ObjectId,
+            ref: 'User',
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        
+        status: Boolean
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
