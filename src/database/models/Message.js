ed054a62c0a8a713ed1585b2fe054fd5d14e563b
franlol <fraaaaan@gmail.com>
2019-12-26 19:43:43 +0100

    :sparkles: - Adding Messages route

--- src/database/models/Message.js
+++ src/database/models/Message.js
@@ -4,27 +4,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-const messageSchema = new Schema(
-    {
-        postId: {
-            type: ObjectId,
-            ref: 'Post',
-            required: true
-        },
-        userId: {
-            type: ObjectId,
-            ref: 'User',
-            required: true
-        },
-        text: {
-            type: String,
-            required: true
-        },
-        status: Boolean
-    },
-    {
-        timestamps: true
-    }
+const messageSchema = new Schema({
+	postId: {
+		type: ObjectId,
+		ref: 'Post',
+		required: true
+	},
+	userId: {
+		type: ObjectId,
+		ref: 'User',
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	status: Boolean
+},
+	{
+		timestamps: true
+	}
 );
 
 const Message = mongoose.model('Message', messageSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const authRouter = require('./auth');
 const profileRouter = require('./profile');
 const postsRouter = require('./posts');
+const messagesRouter = require('./messages');
 
 router.use('/auth', authRouter);
 router.use('/profile', profileRouter);
+router.use('/posts/messages', messagesRouter);
 router.use('/posts', postsRouter);
 
 module.exports = router;
