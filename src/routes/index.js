cbd519a2c2fe0882610df29ba13ff90b28bcdfd5
Miriam <31922082+MiriamAparicio@users.noreply.github.com>
2019-12-29 18:35:29 +0100

    Availability endpoints (#19)
    * Added availability POST, GET, PUT and DELETE endpoints
    
    * Added test to availability route
    
    * add calendar to a constant
    
    * delete console logs

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const authRouter = require('./auth');
 const profileRouter = require('./profile');
 const postsRouter = require('./posts');
+const availabilityRouter = require('./availability');
 const messagesRouter = require('./messages');
 
 router.use('/auth', authRouter);
 router.use('/profile', profileRouter);
 router.use('/posts/messages', messagesRouter);
 router.use('/posts', postsRouter);
+router.use('/availability', availabilityRouter);
 
 module.exports = router;
